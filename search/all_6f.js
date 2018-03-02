a64de20955f00c939140239081cc742d5ec5bcc5
Dwachs <dwachs@gmx.net>
2018-03-02 16:18:03 +0100

    update to r8394

--- search/all_6f.js
+++ search/all_6f.js
@@ -4,7 +4,8 @@ var searchData=
   ['obj_5fdesc_5ftransport_5fx',['obj_desc_transport_x',['../classobj__desc__transport__x.html',1,'']]],
   ['obj_5fdesc_5fx',['obj_desc_x',['../classobj__desc__x.html',1,'']]],
   ['obsolete_5fvehicles_5fallowed',['obsolete_vehicles_allowed',['../classsettings.html#a75a2ba28bbb4ed17b0b89e15d087925e',1,'settings']]],
-  ['open_5finfo_5fwin',['open_info_win',['../classgui.html#abeca0cbb67b1a81f1757563e8f3c665b',1,'gui']]],
-  ['open_5finfo_5fwin_5fat',['open_info_win_at',['../classgui.html#ad0301330af295052343584a0d7f9eefb',1,'gui']]],
+  ['open_5finfo_5fwin',['open_info_win',['../classgui.html#a372a35222439055994fc3498b0286139',1,'gui']]],
+  ['open_5finfo_5fwin_5fat',['open_info_win_at',['../classgui.html#abcbbdae94948d29e8b726fba1eb83b2e',1,'gui']]],
+  ['open_5finfo_5fwin_5fclient',['open_info_win_client',['../classgui.html#a407dd8f68eca07a61ddc3f5dd25391e3',1,'gui']]],
   ['output',['output',['../classfactory__x.html#af68f35c655f0351a50ce6b10c43c7036',1,'factory_x']]]
 ];
