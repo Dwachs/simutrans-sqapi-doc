1ab780a5bf77bb9a4e35ad63242d4e313f79fd65
Dwachs <dwachs@gmx.net>
2018-10-19 18:57:53 +0200

    update doxygen, update doc to r8613

---
+++ search/searchdata.js
@@ -0,0 +1,39 @@
+var indexSectionsWithContent =
+{
+  0: "_abcdefghilmnoprstuvwxyz",
+  1: "abcdefghilmnoprstvw",
+  2: "ac",
+  3: "_abcdefghilmnoprstuw",
+  4: "aefilmnoprstvwxyz",
+  5: "bcmtuw",
+  6: "acdfhmpstw",
+  7: "afs",
+  8: "acd"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "files",
+  3: "functions",
+  4: "variables",
+  5: "enums",
+  6: "enumvalues",
+  7: "groups",
+  8: "pages"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Classes",
+  2: "Files",
+  3: "Functions",
+  4: "Variables",
+  5: "Enumerations",
+  6: "Enumerator",
+  7: "Modules",
+  8: "Pages"
+};
+
